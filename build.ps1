dfc457e4dc9107a161c48ff0ad259a649f8b6231
Austin Hale <haleau@gmail.com>
2023-06-12 00:10:33 -0700

    refactor: specify the config as Release by default since you can still debug from the generated VS debug config

--- build.ps1
+++ build.ps1
@@ -13,13 +13,13 @@ mkdir $buildDir -ErrorAction SilentlyContinue
 Set-Location $buildDir
 
 if ($plat -eq 'UWP') {
-    & cmake -A $arch '-DCMAKE_CXX_FLAGS=/MP' '-DCMAKE_SYSTEM_NAME=WindowsStore' '-DCMAKE_SYSTEM_VERSION=10.0' '-DDYNAMIC_LOADER=OFF' '-Wno-deprecated' '-Wno-dev' ..
+    & cmake -A $arch '-DCMAKE_BUILD_TYPE=Release' '-DCMAKE_CXX_FLAGS=/MP' '-DCMAKE_SYSTEM_NAME=WindowsStore' '-DCMAKE_SYSTEM_VERSION=10.0' '-DDYNAMIC_LOADER=OFF' '-Wno-deprecated' '-Wno-dev' ..
 } else {
-    & cmake -A $arch '-DCMAKE_CXX_FLAGS=/MP' '-DDYNAMIC_LOADER=OFF' '-Wno-deprecated' '-Wno-dev' ..
+    & cmake -A $arch '-DCMAKE_BUILD_TYPE=Release' '-DCMAKE_CXX_FLAGS=/MP' '-DDYNAMIC_LOADER=OFF' '-Wno-deprecated' '-Wno-dev' ..
 }
 
 # Build
-& cmake --build .. -j8
+& cmake --build .. -j8 --config Release
 
 Start-Process SKMediaFoundation.sln
 
